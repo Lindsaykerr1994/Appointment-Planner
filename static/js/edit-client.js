6d1788ac1680d88d792777bae2f33b1ab930f8ef
Lindsaykerr1994 <lindsaykerr1994@gmail.com>
2020-07-06 09:24:23 +0000

    Size responsive styling implemented for base.html

--- static/js/edit-client.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".delete-button").click(updateModalLinks);
-    $(".client-button").mouseenter(function(){
-        $(".client-button i").text("person_add");
-    })
-    $(".client-button").mouseleave(function(){
-        $(".client-button i").text("add");
-    })
-})
